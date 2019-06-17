cc7025fd4b86d8d3ed8be9fc0d3367ecd0e7a9c9
lzx <953934680@qq.com>
2019-06-17 14:56:44 +0800

    redis测试

--- src/main/java/com/lzx/demo/configuration/ActuatorWebSecurityConfigurationAdapter.java
+++ src/main/java/com/lzx/demo/configuration/ActuatorWebSecurityConfigurationAdapter.java
@@ -1,9 +1,9 @@
 package com.lzx.demo.configuration;
 
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 /**
  * 描述:
@@ -11,15 +11,15 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
  * @Auther: lzx
  * @Date: 2019/4/25 15:00
  */
-@Configuration
-@EnableWebSecurity
-public class ActuatorWebSecurityConfigurationAdapter extends WebSecurityConfigurerAdapter {
+//@Configuration
+//@EnableWebSecurity
+public class ActuatorWebSecurityConfigurationAdapter/* extends WebSecurityConfigurerAdapter*/ {
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-                .antMatchers("/swagger-ui.html").authenticated()
-                .and()
-                .formLogin();
-    }
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http.authorizeRequests()
+//                .antMatchers("/swagger-ui.html").authenticated()
+//                .and()
+//                .formLogin();
+//    }
 }
