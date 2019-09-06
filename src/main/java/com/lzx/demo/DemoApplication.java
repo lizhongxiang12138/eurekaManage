be358e8486bd436b3b75bfaab1966423cbbbc78f
lzx <953934680@qq.com>
2019-09-06 17:19:41 +0800

    删除应用按钮

--- src/main/java/com/lzx/demo/DemoApplication.java
+++ src/main/java/com/lzx/demo/DemoApplication.java
@@ -8,7 +8,7 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
 @EnableFeignClients
-@EnableEurekaClient
+//@EnableEurekaClient
 public class DemoApplication {
 
     public static void main(String[] args) {
